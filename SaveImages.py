346bd8664593bc9a21d76a513a4939f41d2cfb75
Bengemon825 <62958342+Bengemon825@users.noreply.github.com>
2020-10-09 02:14:34 -0400

    fixed comments

--- SaveImages.py
+++ SaveImages.py
@@ -13,7 +13,7 @@ async def on_ready():
 
 @client.command()
 async def save(ctx):
-    # USAGE: use command .save in chat when uploading an image to save the image as a jpg
+    # USAGE: use command .save in the comment box when uploading an image to save the image as a jpg
     try:
         url = ctx.message.attachments[0].url            # check for an image, call exception if none found
     except IndexError:
@@ -25,7 +25,7 @@ async def save(ctx):
             imageName = str(uuid.uuid4()) + '.jpg'      # uuid creates random unique id to use for image names
             with open(imageName, 'wb') as out_file:
                 print('Saving image: ' + imageName)
-                shutil.copyfileobj(r.raw, out_file)     # save image (goes to project directory
+                shutil.copyfileobj(r.raw, out_file)     # save image (goes to project directory)
 
 client.run('TOKEN')
 
