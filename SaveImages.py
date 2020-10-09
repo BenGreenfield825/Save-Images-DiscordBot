93606bd0a67da788e4c6e5f49934e716339fc77b
Bengemon825 <62958342+Bengemon825@users.noreply.github.com>
2020-10-09 01:55:22 -0400

    Create SaveImages.py

---
+++ SaveImages.py
@@ -0,0 +1,31 @@
+from discord.ext import commands
+import uuid
+import requests
+import shutil
+
+client = commands.Bot(command_prefix='.')
+
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+
+
+@client.command()
+async def save(ctx):
+    # USAGE: use command .save in chat when uploading an image to save the image as a jpg
+    try:
+        url = ctx.message.attachments[0].url            # check for an image, call exception if none found
+    except IndexError:
+        print("Error: No attachments")
+        await ctx.send("No attachments detected!")
+    else:
+        if url[0:26] == "https://cdn.discordapp.com":   # look to see if url is from discord
+            r = requests.get(url, stream=True)
+            imageName = str(uuid.uuid4()) + '.jpg'      # uuid creates random unique id to use for image names
+            with open(imageName, 'wb') as out_file:
+                print('Saving image: ' + imageName)
+                shutil.copyfileobj(r.raw, out_file)     # save image (goes to project directory
+
+client.run('TOKEN')
+
